0db9f562b7351dca50658dd2df3779fa7420fab7
guy michaely <michaelyguy@gmail.com>
2020-06-26 11:22:05 +0200

    part 2

--- index.js
+++ index.js
@@ -4,12 +4,54 @@ const app = express();
 app.use(express.static("public"));
 const { getUrlAndTitle } = require("./db.js");
 
+////// FILE UPLOAD BOILERPLATE //////
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+//////////////////////////////////////
+
 app.get("/images", (req, res) => {
     getUrlAndTitle().then((result) => {
         console.log("------RESULT IN /IMAGES------");
         console.log(result);
         res.json(result.rows);
     });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    //// REQ.FILE IS THE FILE WE JUST UPLOADED
+    console.log("file: ", req.file);
+    //// REQ.BODY IS THE REST OF THE INPUT FIELD -username , description
+    console.log("input: ", req.body);
+
+    if (req.file) {
+        /// MAKE DB INSERT FOR ALL THE INFO ////
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
 });
 
 app.listen(8080, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -5,20 +5,50 @@
         data: {
             seen: false,
             images: [],
+            // ADDING DATA PROPERTIES THAT WILL STORE FOR THE IMPUT FIELD
+            title: "",
+            description: "",
+            username: "",
+            file: null,
         },
 
         mounted: function () {
             var self = this;
             axios.get("/images").then(function (response) {
                 console.log("this: ", self);
-                console.log("response from cities:", response);
+                console.log("response from images:", response);
                 self.images = response.data;
             });
         },
 
         methods: {
-            myFunction: function () {
-                console.log("my function is running!!!");
+            /// "THIS" IS YOUT FRIEND ////
+            handleClick: function (event) {
+                event.preventDefault();
+                console.log("this:", this);
+
+                //// WE USE FORMDATA ONLY COS WE ARE WORKING WITH A FILE!! ///
+                var formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("desctiption", this.desctiption);
+                formData.append("user", this.username);
+                formData.append("file", this.file);
+
+                axios
+                    .post("/upload", formData)
+                    .then(function (response) {
+                        console.log("this is the response: ", response);
+                    })
+                    .catch(function (err) {
+                        console.log("error ins POST /upload: ", err);
+                    });
+            },
+            handleChange: function (event) {
+                console.log("handleChange is running!");
+                console.log("file: ", event.target.files[0]);
+
+                this.file = event.target.files[0];
+                console.log("this after adding file to data:", this);
             },
         },
     });
